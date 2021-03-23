1da9fccfa0e38044a52f9fff85fee6f3a76fd8fd
disizaniknem <disizaniknem@gmail.com>
2021-03-23 14:45:22 +0100

    Gestion des commentaires

--- routes/post.routes.js
+++ routes/post.routes.js
@@ -9,5 +9,8 @@ router.patch('/like-post/:id', postController.likePost)
 router.patch('/unlike-post/:id', postController.unlikePost)
 
 // comments
+router.patch('/comment-post/:id', postController.commentPost)
+router.patch('/edit-comment-post/:id', postController.editCommentPost)
+router.patch('/delete-comment-post/:id', postController.deleteCommentPost)
 
 module.exports = router
