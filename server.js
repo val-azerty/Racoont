3a08b115ef814e083a12474c04ca43f58b0e3d3f
disizaniknem <disizaniknem@gmail.com>
2021-03-23 16:45:01 +0100

    Ajout de cors

--- server.js
+++ server.js
@@ -2,11 +2,22 @@ const express = require('express')
 const cookieParser = require('cookie-parser')
 const userRoutes = require('./routes/user.routes')
 const postRoutes = require('./routes/post.routes')
-require('dotenv').config({path: './config/.env'})
+require('dotenv').config({ path: './config/.env' })
 require('./config/db')
-const {checkUser, requireAuth} = require('./middleware/auth.middleware')
-const app = express();
+const { checkUser, requireAuth } = require('./middleware/auth.middleware')
+const cors = require('cors')
+const app = express()
 
+const corsOptions = {
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+    'allowedHeaders': ['sessionId', 'Content-Type'],
+    'exposedHeaders': ['sessionId'],
+    'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    'preflightContinue': false
+}
+
+app.use(cors(corsOptions))
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
 app.use(cookieParser())
@@ -24,4 +35,4 @@ app.use('/api/post', postRoutes)
 // server
 app.listen(process.env.PORT, () => {
     console.log(`Listening on port ${process.env.PORT}`)
-}) 
+})
