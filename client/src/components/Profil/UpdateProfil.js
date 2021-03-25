8077e6b18e94c1c046e01d524f5fe249422906f0
disizaniknem <disizaniknem@gmail.com>
2021-03-25 12:43:38 +0100

    Modification de la bio

--- client/src/components/Profil/UpdateProfil.js
+++ client/src/components/Profil/UpdateProfil.js
@@ -1,25 +1,58 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
-import LeftNav from '../LeftNav'
-import UploadImg from './UploadImg'
+import React, { useState } from "react"
+import { useSelector, useDispatch } from "react-redux"
+import { updateBio } from "../../actions/user.actions"
+import LeftNav from "../LeftNav"
+import UploadImg from "./UploadImg"
 
 const UpdateProfil = () => {
+   const [bio, setBio] = useState("")
+   const [updateForm, setUpdateForm] = useState(false)
+   const userData = useSelector((state) => state.userReducer)
+   const dispatch = useDispatch()
 
-    const userData = useSelector((state) => state.userReducer)
+   const handleUpdate = (e) => {
+      dispatch(updateBio(userData._id, bio))
+      setUpdateForm(false)
+   }
 
-    return (
-        <div className="profil-container">
-            <LeftNav/>
-            <h1>Profil de {userData.pseudo}</h1>
-            <div className="update-container">
-                <div className="left-part">
-                    <h3>Photo de profil</h3>
-                    <img src={userData.picture} alt="user-pic"/>
-                    <UploadImg/>
-                </div>
+   return (
+      <div className="profil-container">
+         <LeftNav />
+         <h1>Profil de {userData.pseudo}</h1>
+         <div className="update-container">
+            <div className="left-part">
+               <h3>Photo de profil</h3>
+               <img src={userData.picture} alt="user-pic" />
+               <UploadImg />
             </div>
-        </div>
-    )
+            <div className="right-part">
+               <h3>Bio</h3>
+               {updateForm === false && (
+                  <>
+                     <p onClick={() => setUpdateForm(!updateForm)}>
+                        {userData.bio}
+                     </p>
+                     <button onClick={() => setUpdateForm(!updateForm)}>
+                        Modifier bio
+                     </button>
+                  </>
+               )}
+               {updateForm && (
+                  <>
+                     <textarea
+                        type="text"
+                        defaultValue={userData.bio}
+                        onChange={(e) => setBio(e.target.value)}
+                     ></textarea>
+                     <button onClick={handleUpdate}>
+                        Valider modifications
+                     </button>
+                  </>
+               )}
+            </div>
+         </div>
+      </div>
+   )
 }
 
 export default UpdateProfil
--- client/src/reducers/user.reducer.js
+++ client/src/reducers/user.reducer.js
@@ -1,17 +1,22 @@
-import { GET_USER, UPLOAD_PICTURE } from "../actions/user.actions"
+import { GET_USER, UPDATE_BIO, UPLOAD_PICTURE } from "../actions/user.actions"
 
 const initialState = {}
 
 export default function userReducer(state = initialState, action) {
-    switch (action.type) {
-        case GET_USER:
-            return action.payload
-        case UPLOAD_PICTURE:
-            return {
-                ...state,
-                picture: action.payload
-            }
-        default:
-            return state
-    }
+   switch (action.type) {
+      case GET_USER:
+         return action.payload
+      case UPLOAD_PICTURE:
+         return {
+            ...state,
+            picture: action.payload,
+         }
+      case UPDATE_BIO:
+         return {
+            ...state,
+            bio: action.payload,
+         }
+      default:
+         return state
+   }
 }
